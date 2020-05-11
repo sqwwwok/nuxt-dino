0e9e8114a054bac5bce094f50a8f85646ef87e52
sqwww <2569362147@qq.com>
2020-05-11 17:50:42 +0800

    撤销路由检查方便测试

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,15 @@
 export default function ( {app,redirect,store,route} ) {
-  if(!store.state.userData.username){
-    app.$axios.get('/login')
-    .then(res => {
-      if(route.path==='/login-register') redirect('/');
-      store.commit({
-        type: 'recordUser',
-        username: res.data.username,
-        historys: res.data.historys,
-      })
-    }).catch(e => {
-      if(route.path!=='/login-register') redirect('/login-register')
-    })
-  }
+  // if(!store.state.userData.username){
+  //   app.$axios.get('/login')
+  //   .then(res => {
+  //     if(route.path==='/login-register') redirect('/');
+  //     store.commit({
+  //       type: 'recordUser',
+  //       username: res.data.username,
+  //       historys: res.data.historys,
+  //     })
+  //   }).catch(e => {
+  //     if(route.path!=='/login-register') redirect('/login-register')
+  //   })
+  // }
 }
